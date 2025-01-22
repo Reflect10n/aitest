d71d9db4c7256a86a3766cc3fd71275719837da5
Муслимов Булат Ильшатович <Muslimov_BI@rosa.com>
2025-01-22 09:33:32 +0400

    123

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -8,8 +8,16 @@ namespace ConsoleApp5
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
+        static void Main(string[] args) {
+            int a = 10;
+            int b = 10;
+            double ac = System.Math.Pow(a, 2);
+            double bc = System.Math.Pow(b, 2);
+            char[] c = a.ToString().ToCharArray();
+            foreach (var  c2 in c) {
+                Console.Write(c2);
+                Console.Write(' ');
+                }
         }
     }
 }
